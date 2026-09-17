e8a4f5356f824fd3e5aa2cfa712635bdc31c2173
Evgeniy Kucher <evgen-kucher@ukr.net>
2026-09-18 02:07:06 +0300

    feat: separate administrator-class user management

--- internal/platformdb/migrations/012_identity_admin_manage.sql
+++ internal/platformdb/migrations/012_identity_admin_manage.sql
@@ -1,5 +1,6 @@
 INSERT INTO permissions (id,description) VALUES
-('identity.user.manage','Create and manage human user accounts')
+('identity.user.manage','Create and manage human user accounts'),
+('identity.user.admin','Manage administrator-class human accounts')
 ON CONFLICT (id) DO UPDATE SET description=EXCLUDED.description;
 
 INSERT INTO role_permissions (role_id,permission_id) VALUES
